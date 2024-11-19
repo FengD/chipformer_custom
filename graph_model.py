c567ace0dbcccc0e37b49b29299f55d31e5893a7
FengD <dingbobby@hotmail.com>
2024-11-19 13:57:08 +0800

    comment the graph model

--- graph_model.py
+++ graph_model.py
@@ -6,26 +6,13 @@ import numpy as np
 
 import graph_args as args
 
-class VGAE(nn.Module):
-	def __init__(self, adj):
-		super(VGAE,self).__init__()
-		self.base_gcn = GraphConvSparse(args.input_dim, args.hidden1_dim, adj)
-		self.gcn_mean = GraphConvSparse(args.hidden1_dim, args.hidden2_dim, adj, activation=lambda x:x)
-		self.gcn_logstddev = GraphConvSparse(args.hidden1_dim, args.hidden2_dim, adj, activation=lambda x:x)
-
-	def encode(self, X):
-		hidden = self.base_gcn(X)
-		self.mean = self.gcn_mean(hidden)
-		self.logstd = self.gcn_logstddev(hidden)
-		gaussian_noise = torch.randn(X.size(0), args.hidden2_dim)
-		sampled_z = gaussian_noise*torch.exp(self.logstd) + self.mean
-		return sampled_z
-
-	def forward(self, X):
-		Z = self.encode(X)
-		A_pred = dot_product_decode(Z)
-		return A_pred
-
+# GCN
+# f(X) = activation(A` X W)
+# A = (adjecent matrix, defines the relation of Nodes in Graph) A` = A + I
+# W weight
+# X input
+# f(X) output
+# https://zhuanlan.zhihu.com/p/633419078
 class GraphConvSparse(nn.Module):
 	def __init__(self, input_dim, output_dim, adj, activation = F.relu, **kwargs):
 		super(GraphConvSparse, self).__init__(**kwargs)
@@ -45,6 +32,10 @@ def dot_product_decode(Z):
 	A_pred = torch.sigmoid(torch.matmul(Z,Z.t()))
 	return A_pred
 
+# Understanding the difficulty of training deep feedforward neural networks
+# 2010 Xavier Glorot, Yoshua Bengio
+# init_range define the uniform range
+# change the input output matrix to nn.Parameter
 def glorot_init(input_dim, output_dim):
 	init_range = np.sqrt(6.0/(input_dim + output_dim))
 	initial = torch.rand(input_dim, output_dim)*2*init_range - init_range
@@ -67,3 +58,23 @@ class GAE(nn.Module):
 		A_pred = dot_product_decode(Z)
 		return A_pred
 		
+# https://arxiv.org/pdf/1611.07308
+class VGAE(nn.Module):
+	def __init__(self, adj):
+		super(VGAE,self).__init__()
+		self.base_gcn = GraphConvSparse(args.input_dim, args.hidden1_dim, adj)
+		self.gcn_mean = GraphConvSparse(args.hidden1_dim, args.hidden2_dim, adj, activation=lambda x:x)
+		self.gcn_logstddev = GraphConvSparse(args.hidden1_dim, args.hidden2_dim, adj, activation=lambda x:x)
+
+	def encode(self, X):
+		hidden = self.base_gcn(X)
+		self.mean = self.gcn_mean(hidden)
+		self.logstd = self.gcn_logstddev(hidden)
+		gaussian_noise = torch.randn(X.size(0), args.hidden2_dim)
+		sampled_z = gaussian_noise*torch.exp(self.logstd) + self.mean
+		return sampled_z
+
+	def forward(self, X):
+		Z = self.encode(X)
+		A_pred = dot_product_decode(Z)
+		return A_pred
